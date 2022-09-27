3ad7ed97dd1502df0cc6720b920c5bc5509b29af
王建文 <83105729@qq.com>
2022-09-27 17:53:41 +0800

    springcloud 微服务框架测试版   本地测试冲突

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/sxdl/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/sxdl/springcloud/OrderHystrixMain80.java
@@ -22,7 +22,6 @@ public class OrderHystrixMain80 {
         InetAddress inetAddress=InetAddress.getLocalHost();
         String ip=inetAddress.getHostAddress();//获得本机Ip
         System.out.println("http://"+ip+":"+prot);
-        System.out.println("代码完美合并完成");
     }
 
 }
